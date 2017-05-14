b0e7b6fd0354b57bd032f960ef4697221c31e19c
MITLAB-Ian <banana950081@gmail.com>
2017-05-14 21:39:24 +0800

    chap4 test

--- app/code/local/Magentotutorial/Helloworld/controllers/IndexController.php
+++ app/code/local/Magentotutorial/Helloworld/controllers/IndexController.php
@@ -2,6 +2,8 @@
 	class Magentotutorial_Helloworld_IndexController extends Mage_Core_Controller_Front_Action {        
 	    public function indexAction() {
 	        echo 'Hello World';
+			   $this->loadLayout();
+    			$this->renderLayout();
 	    }
             public function googbyeAction()
             {
